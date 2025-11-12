cb7a6195fcaa2f45b0c3ced9b791a17bd1b1b406
Ela <ela.kazemzadeh@gmail.com>
2025-11-12 17:14:45 +0100

    Added Flight booking fullname function

---
+++ javascript/week2/flight-booking-fullname.js
@@ -0,0 +1,12 @@
+function getFullName(firstName, surname) {
+    
+    const fullName = `${firstName} ${surname}`;
+   
+    return fullName;
+}
+const fullName1 = getFullName("Kate", "Wilson");
+const fullName2 = getFullName("Alex", "Hens");
+
+console.log(fullName1);
+console.log(fullName2);
+
