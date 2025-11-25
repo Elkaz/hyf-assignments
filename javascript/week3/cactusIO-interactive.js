4908e204be9b4c5c7273f01471f779640146df97
Ela <ela.kazemzadeh@gmail.com>
2025-11-25 12:32:52 +0100

    Updated Smart phone usage app

--- javascript/week3/cactusIO-interactive.js
+++ javascript/week3/cactusIO-interactive.js
@@ -1,61 +1,43 @@
 const activities = [];
-const usageLimit = 60; 
+const usageLimit = 60;
 
 //Adding an activity
 
 function addActivity(date, activity, duration) {
-    let totalTime = 0;
-    for (let i =0; i < activities.length; i++) {
-        totalTime += activities[i].duration;
-    }
-    
-   //Usage limit : I could't find the right place for usage limit. I know I have to check it before the push but
-   // I get the limit reached error before getting the status. 
-
-    if (totalTime + duration > usageLimit) {
-        console.log(`You have reached your limit, no more smart phoning for you!`)
-        return;
-    }
-
-     activities.push({
-        date: date,
-        activity: activity,
-        duration: duration
-    });
+  activities.push({
+    date: date,
+    activity: activity,
+    duration: duration,
+  });
 }
 
 //Show my status
 
-function showStatus(activities) {
-    if (activities.length === 0) {
-        console.log(`Add some activities before calling showStatus.`);
-        return;
+function showStatus() {
+  if (!activities.length) {
+    console.log("Add some activities before calling showStatus");
+    return;
+  }
 
-    }
-    let totalTime = 0;
-    for (let i = 0; i < activities.length; i++) {
-    totalTime += activities[i].duration;
-    }
-   
-    const numberOfActivity = activities.length;
-    console.log(`You have added ${numberOfActivity} activities. They amount to ${totalTime} min. of usage.`);
+  const numberOfActivities = activities.length;
 
+  let totalDuration = 0;
+  for (let i = 0; i < activities.length; i++) {
+    totalDuration = totalDuration + activities[i].duration;
+  }
+
+  if (totalDuration > usageLimit) {
+    return `You have reached your limit, no more smart phoning for you!`;
+  }
+
+  return `You have added ${numberOfActivities} activities. They amount to ${totalDuration} min. of usage.`;
 }
+
+console.log(activities);
 
 addActivity("23/7-18", "Youtube", 30);
 addActivity("23/7-18", "TikTok", 10);
+console.log(showStatus());
 addActivity("23/7-18", "Instagram", 20);
 addActivity("23/7-18", "Apple music", 30);
-
-
-showStatus(activities); 
-console.log(activities);
-
-
-
-
-
-
-
-
- 
+console.log(showStatus());
