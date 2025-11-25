833866316b252d72c640bc32007258995751acc0
Ela <ela.kazemzadeh@gmail.com>
2025-11-25 12:28:49 +0100

    Fixed Note taking app

--- javascript/week3/note-taking-app.js
+++ javascript/week3/note-taking-app.js
@@ -3,30 +3,32 @@
 const notes = [];
 
 function saveNote(content, id) {
-  notes.push({content: content, id:id});
+  notes.push({ content: content, id: id });
 }
 
 saveNote("Pick up groceries", 1);
 saveNote("Do laundry", 2);
 
-console.log(notes); 
+console.log(notes);
 
 //Get a note
 
 function getNote(id) {
-  if(id === undefined || typeof id !== "number") {
+  if (!id || typeof id !== "number") {
     console.log("Error: Please write a valid id.");
     return;
   }
   for (let i = 0; i < notes.length; i++) {
-    if(notes[i].id == id) {
-        return notes[i];
+    if (notes[i].id == id) {
+      return notes[i];
     }
   }
+  console.log("Error: Note not found.");
+  return;
 }
 
 const firstNote = getNote(1);
-console.log(firstNote); 
+console.log(firstNote);
 
 const letterNote = getNote("a");
 console.log(letterNote);
@@ -34,21 +36,22 @@ console.log(letterNote);
 //Log out notes
 
 function logOutNotesFormatted() {
-  
- notes.forEach(note => {
-    console.log(`The note with id: ${note.id} has the following note text: ${note.content}.`)
- });
+  notes.forEach((note) => {
+    console.log(
+      `The note with id: ${note.id} has the following note text: ${note.content}.`
+    );
+  });
 }
 
-logOutNotesFormatted(); 
+logOutNotesFormatted();
 
 //Search feature
 // I like to add search feature by a keyword to the app.
 
 function searchNotes(keyword) {
-
-    return notes.filter(note => note.content.toLowerCase().includes(keyword.toLowerCase()));
-
+  return notes.filter((note) =>
+    note.content.toLowerCase().includes(keyword.toLowerCase())
+  );
 }
 
 console.log(searchNotes("Pick"));
