cd9ea5fdb84f91d2ad046d4884b606e50ed3926a
Ela <ela.kazemzadeh@gmail.com>
2026-03-11 19:36:00 +0100

    Low stock alert

---
+++ advanced-javascript/week1/exercise3.js
@@ -0,0 +1,11 @@
+import { teas as data } from "./teas.js";
+
+function lowStockAlert(teas) {
+  
+  return teas
+    .filter((tea) => tea.stockCount < 50)
+    .map((tea) => ({ name: tea.name, stockCount: tea.stockCount }))
+    .sort((a, b) => a.stockCount - b.stockCount);
+}
+
+console.log(lowStockAlert(data));
