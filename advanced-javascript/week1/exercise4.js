131c9c5cce27d3c62df603d4459b9935a0a58828
Ela <ela.kazemzadeh@gmail.com>
2026-03-18 11:24:22 +0100

    Fix formatting

--- advanced-javascript/week1/exercise4.js
+++ advanced-javascript/week1/exercise4.js
@@ -1,16 +1,15 @@
 import { teas } from "./teas.js";
 
 function teasByOrigin(teas) {
-return teas.reduce((result, tea) => {
-const origin = tea.origin;
- if (!result[origin]) {
+  return teas.reduce((result, tea) => {
+    const origin = tea.origin;
+    if (!result[origin]) {
       result[origin] = [];
- }
+    }
     result[origin].push(tea.name);
 
     return result;
-}, {});
-
-  }
+  }, {});
+}
 
 console.log(teasByOrigin(teas));
